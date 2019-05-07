d237b582278c1bb34df11de058c189bcc47df239
heitorcampo2 <heitormtb12@gmail.com>
2019-05-07 19:46:08 -0300

    v1

--- src/Data.java
+++ src/Data.java
@@ -1,7 +1,22 @@
 public class Data {
-    int dia;
-    int mes;
-    int ano;
+    private int dia;
+    private int mes;
+    private int ano;
     
+    public int getDia(){
+        return this.dia;
+    }
+    
+    public int getMes(){
+        return this.dia;
+    }
+        
+    public int getAno(){
+        return this.dia;
+    }
+    
+    public void mostrarData(){
+        System.out.println(this.getDia()+"/"+this.getMes()+"/"+this.getAno());
+    }
     
 }
