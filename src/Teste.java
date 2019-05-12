b38065d6cf5fecc607d08a8249cecea2282bb060
heitorcampos2 <heitormtb12@gmail.com>
2019-05-12 11:54:58 -0300

    v3

--- src/Teste.java
+++ src/Teste.java
@@ -1,8 +1,27 @@
 public class Teste {
     public static void main(String[] args){
+        /////////////////////////////////////
+        Pessoa p1 = new Pessoa();
+        p1.setNome("Fulano");
         
+        Data p01 = new Data();
+        p01.getDia(05);
+        p01.getMes(08);
+        p01.getAno(1997);
+        p1.setDataDeNascimento(p01);
+        /////////////////////////////////////
+        Veiculo l = new Veiculo();
+        l.setProprietario(p1);
         
-        
+        Data da = new Data();
+        da.getDia(01);
+        da.getMes(02);
+        da.getAno(2019);
+        l.getDtaAquisicao(da);
+        /////////////////////////////////////
+        p1.MostrarPessoa();
+        l.InformarFinalidade("Passeio");
+        l.MostrarVeiculo();
         
     }
 }
