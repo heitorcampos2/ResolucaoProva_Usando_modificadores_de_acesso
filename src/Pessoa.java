1e74856d0e530ecb5fd33d1123572f3eb9acfb1f
heitorcampos2 <heitormtb12@gmail.com>
2019-05-12 11:33:29 -0300

    v2

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,7 +1,7 @@
 public class Pessoa {
     private Data dataDeNascimento;
     private String nome;
-    
+    /////////////////////////////////////
     public Data getDataDeNascimento(){
         return this.dataDeNascimento;
     }
@@ -9,11 +9,19 @@ public class Pessoa {
     public String getNome(){
         return this.nome;
     }
+    /////////////////////////////////////
+    public void setNome(String n){
+        this.nome=n;
+    }
     
+    public void setDataDeNascimento(Data d){
+        this.dataDeNascimento = d;
+    }
+    /////////////////////////////////////
     public void MostrarPessoa(){
         System.out.println("Pessoa: "+this.getNome());
         System.out.println("Data de nascimento: ");
         this.getDataDeNascimento().mostrarData();
     }
-    
+    /////////////////////////////////////
 }
