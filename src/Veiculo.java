373bcfc14887574934348dd35079ca9958b2ceb1
heitorcampos2 <heitormtb12@gmail.com>
2019-05-12 12:04:41 -0300

    v3

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,3 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author HeiThor
+ */
 public class Veiculo {
     private Data dataAquisicao;
     private Pessoa proprietario;
